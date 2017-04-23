eb052ca6389fd704dbd3f08523dd4519f1314cc2
aidanjenkins2016 <aidanjenkins2016@u.northwestern.edu>
2017-04-23 16:41:17 -0500

    transmitting x and y coordinate floats, good performance at approx 1 to 2 Hz

--- TeensyXBeeRx/TeensyXBeeRx.ino
+++ TeensyXBeeRx/TeensyXBeeRx.ino
@@ -1,17 +1,18 @@
 //Aidan Jenkins
 //April 20, 2017
 
-//Sample code for receiving a broadcast message on XBee 
+//Sample code for receiving a broadcast message on XBee
 //radios in AT (transparent) mode
 
-//#include <XBee.h>
 #include <SoftwareSerial.h>
+
+boolean serialDebug=true;
 
 //Software serial pins on your microcontroller
 int sstx = 10; //tx2=10;
 int ssrx = 9; //rx2=9
+
 SoftwareSerial ss(ssrx, sstx); //serial comms for xbee and teensy
-//XBee xbee = XBee();
 
 void setup() {
   // put your setup code here, to run once:
@@ -19,13 +20,20 @@ void setup() {
   Serial.begin(9600); //optional serial monitoring on your computer
 }
 
-string received;
+String xpos, ypos;
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if (ss.available()) {
-    received = ss.read();
-    Serial.println(received);
+  while (ss.available() > 0) {
+    xpos = ss.readStringUntil(',');
+    //ss.read();
+    ypos = ss.readStringUntil('\n');
+
+    if (serialDebug) {
+      Serial.print( xpos.toFloat());
+      Serial.print(", ");
+      Serial.println(ypos.toFloat() );
+    }
   }
 
 }
--- TeensyXBeeTx/TeensyXBeeTx.ino
+++ TeensyXBeeTx/TeensyXBeeTx.ino
@@ -1,11 +1,18 @@
+//Aidan Jenkins
+//April 20, 2017
+
+//Sample code for transmitting a broadcast message on XBee
+//radios in AT (transparent) mode
+
 //#include <XBee.h>
 #include <SoftwareSerial.h>
 
-int sstx=10; //tx2=10;
-int ssrx=9; //rx2=9
+boolean serialDebug = true;
 
-SoftwareSerial ss(ssrx,sstx); //serial comms for xbee and teensy
-//XBee xbee=XBee();
+int sstx = 10; //tx2=10;
+int ssrx = 9; //rx2=9
+
+SoftwareSerial ss(ssrx, sstx); //serial comms for xbee and teensy
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,19 +20,22 @@ void setup() {
   Serial.begin(9600);
 }
 
-int j=1;
+float xpos = 1.59;
+float ypos = 2.82;
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  //ss.write(j);
-  ss.print(j);
-  ss.print(",");
-  ss.println(j);
 
-  Serial.print(j);
-  Serial.print(",");
-  Serial.println(j);
-  
-  j=j+1;
-  delay(1000);
+  //ss.write(j);
+  ss.print(xpos);
+  ss.print(", ");
+  ss.println(ypos);
+
+  if (serialDebug) {
+    Serial.print(xpos);
+    Serial.print(", ");
+    Serial.println(ypos);
+  }
+  xpos = xpos + 1;
+  ypos = ypos + 1;
+ 
 }
